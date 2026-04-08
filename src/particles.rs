cb4a42afbcce312293469cbd2a796184f8762804
GldeA <glde.abbott@gmail.com>
2026-04-08 14:58:21 -0600

    Implemented sand physics

--- src/particles.rs
+++ src/particles.rs
@@ -1,12 +1,10 @@
 pub mod particles {
     use raylib::color::Color;
 
-    use crate::Vector2i;
+    use crate::{Grid, Vector2i};
 
 	pub trait Particle {
-		fn update_position(&mut self) {
-
-		}
+		fn update_position(&self, position: Vector2i, _grid: &Grid) -> Vector2i { position }
 		fn get_color(&self) -> Color;
 	}
 
@@ -15,6 +13,30 @@ pub mod particles {
 		pub fn new() -> Sand { Sand { } }
 	}
 	impl Particle for Sand {
+		fn update_position(&self, position: Vector2i, grid: &Grid) -> Vector2i {
+			if grid.is_in_bounds(position + Vector2i::new(0, 1)) && grid.get(position + Vector2i::new(0, 1)).is_none() {
+				return position + Vector2i::new(0, 1);
+			}
+			let check_left_first = rand::random_bool(0.5);
+			if check_left_first {
+				if grid.is_in_bounds(position + Vector2i::new(-1, 1)) && grid.get(position + Vector2i::new(-1, 1)).is_none() {
+					return position + Vector2i::new(-1, 1);
+				}
+				else if grid.is_in_bounds(position + Vector2i::new(1, 1)) && grid.get(position + Vector2i::new(1, 1)).is_none() {
+					return position + Vector2i::new(1, 1);
+				}
+			}
+			else {
+				if grid.is_in_bounds(position + Vector2i::new(1, 1)) && grid.get(position + Vector2i::new(1, 1)).is_none() {
+					return position + Vector2i::new(1, 1);
+				}
+				else if grid.is_in_bounds(position + Vector2i::new(-1, 1)) && grid.get(position + Vector2i::new(-1, 1)).is_none() {
+					return position + Vector2i::new(-1, 1);
+				}
+			}
+			return position;
+		}
+
 		fn get_color(&self) -> Color { Color::GOLD }
 	}
 }
